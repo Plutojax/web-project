7b18b7d91a68090e29144840013c72b4a2b5da75
Yifei Wang <ywang825@sheffield.ac.uk>
2023-05-19 02:30:02 +0100

    upload

--- controllers/api/get_post.js
+++ controllers/api/get_post.js
@@ -6,7 +6,7 @@ const Sighting = require('../../model/Sighting');
 const getAllSightings = async (req, res) => {
     console.log('Reached inside getAllSightings in controllers/api');
     try {
-        const posts = await Sighting.find({}, { image: 1, _id: 1,DateSeen:1,Identification:1 }).sort("DateSeen");
+        const posts = await Sighting.find({}, { image: 1, _id: 1,DateSeen:1,Identification:1,Description:1,location:1 }).sort("DateSeen");
         res.status(200).json(posts);
     } catch (error) {
         res.status(500).json({ message: error.message });
